0fdabfa0d304088480cec86d86045837de866b8c
Demir <demirince@ucsb.edu>
2024-01-30 17:23:30 -0800

    HomeController and Tests done

--- src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/HomeControllerTests.java
+++ src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/HomeControllerTests.java
@@ -49,12 +49,14 @@ public class HomeControllerTests {
         String baseUrl = "http://localhost:8080/";
 
         List<String> team = new ArrayList<String>();
-        team.add("Jing P.");
-        team.add("Christian S.");
-        team.add("Guy W.");
-        team.add("Phill C.");
+        team.add("Demir I.");
+        team.add("Kevin L.");
+        team.add("Vincent Z.");
+        team.add("Tienan V.");
+        team.add("Mihir B.");
+        team.add("Jeff C.");
         resultMap.put("team", team);
-        resultMap.put("repo", "https://github.com/ucsb-cs156-w24/STARTER-team01");
+        resultMap.put("repo", "https://github.com/ucsb-cs156-w24/team01-w24-6pm-2");
         resultMap.put("api-documentation", baseUrl + "swagger-ui/index.html");
         String expected = mapper.writeValueAsString(resultMap);
         String actual = HomeController.getHomePageObjectJSON("http://localhost:8080/");
